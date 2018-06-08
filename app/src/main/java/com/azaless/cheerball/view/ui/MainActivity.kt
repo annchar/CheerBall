cf3ca15d56d621c244a3729301e203332ea85ea8
AnnChar <ann.skr14@gmail.com>
2018-06-08 15:27:26 +0700

    Replace fragment in drawer

--- app/src/main/java/com/azaless/cheerball/view/ui/MainActivity.kt
+++ app/src/main/java/com/azaless/cheerball/view/ui/MainActivity.kt
@@ -2,24 +2,25 @@ package com.azaless.cheerball.view.ui
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.azaless.cheerball.R
+import com.azaless.cheerball.view.ui.groupsball.GroupsBallFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 	private lateinit var drawerLayout: DrawerLayout
 	private lateinit var drawerToggle: ActionBarDrawerToggle
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
-		if (savedInstanceState == null)
-			supportFragmentManager.beginTransaction()
-				.add(R.id.main_fragment, MainFragment.newInstance())
-				.commit()
 
 		setupToolbar()
 		setupNavigationDrawer()
@@ -38,14 +39,17 @@ class MainActivity : AppCompatActivity() {
 		drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close)
 		drawerLayout.addDrawerListener(drawerToggle)
 
-		// TODO: Set navigator
-//		val navController = Navigation.findNavController(this, R.id.main_fragment)
-//		findViewById<NavigationView>(R.id.navigation_view).setupWithNavController(navController)
+		val navController = Navigation.findNavController(this, R.id.main_fragment)
+		val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+		navigationView.setupWithNavController(navController)
+		navigationView.setNavigationItemSelectedListener(this)
+		navigationView.setCheckedItem(R.id.nav_home)
 	}
 
 	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
 		if (drawerToggle.onOptionsItemSelected(item))
 			return true
+
 		return super.onOptionsItemSelected(item)
 	}
 
@@ -58,4 +62,31 @@ class MainActivity : AppCompatActivity() {
 		super.onConfigurationChanged(newConfig)
 		drawerToggle.onConfigurationChanged(newConfig)
 	}
+
+	override fun onNavigationItemSelected(item: MenuItem): Boolean {
+		var fragment : Fragment? = null
+		when(item.itemId){
+			R.id.nav_groups -> {
+				fragment = GroupsBallFragment.newInstance()
+			}
+			R.id.nav_all_teams -> {
+
+			}
+			else -> {
+				fragment = MainFragment.newInstance()
+			}
+
+		}
+
+		fragment?.let {
+			supportFragmentManager.beginTransaction()
+				.replace(R.id.main_fragment, fragment)
+				.addToBackStack(null)
+				.commit()
+		}
+
+		drawerLayout.closeDrawer(GravityCompat.START)
+		return true
+	}
+
 }
