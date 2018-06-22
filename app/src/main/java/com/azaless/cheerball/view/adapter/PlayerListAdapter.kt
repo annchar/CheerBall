bfa357180bb51fa0ec853a201310bcfa85587566
AnnChar <ann.skr14@gmail.com>
2018-06-22 14:43:17 +0700

    Set playerAdapter

--- app/src/main/java/com/azaless/cheerball/view/adapter/PlayerListAdapter.kt
+++ app/src/main/java/com/azaless/cheerball/view/adapter/PlayerListAdapter.kt
@@ -1,18 +1,20 @@
 package com.azaless.cheerball.view.adapter
 
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.azaless.cheerball.R
+import com.azaless.cheerball.databinding.ListItemPlayerBinding
 import com.azaless.cheerball.view.model.Player
+
 
 class PlayerListAdapter : DataBindingAdapter<List<Player>, PlayerListAdapter.ViewHolder>() {
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-		return ViewHolder(LayoutInflater.from(parent.context)
-			.inflate(R.layout.list_item_player, parent, false))
+		val layoutInflater = LayoutInflater.from(parent.context)
+		val binding = DataBindingUtil.inflate<ListItemPlayerBinding>(layoutInflater, R.layout.list_item_player, parent, false)
+		return ViewHolder(binding)
 	}
 
 	override fun getItemCount() = values?.size ?: 0
@@ -20,20 +22,15 @@ class PlayerListAdapter : DataBindingAdapter<List<Player>, PlayerListAdapter.Vie
 	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 		with(holder) {
 			values?.let { values ->
-				textViewName.text = values[position].name
-				textViewPosition.text = values[position].position
-				textViewNumber.text = values[position].jerseyNumber.toString()
-//			    textViewAge.text = values[position].
-				textViewDateOfBirth.text = values[position].dateOfBirth
+				holder.bind(values[position])
 			}
 		}
 	}
 
-	class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-		val textViewName: TextView = itemView.findViewById(R.id.textViewName)
-		val textViewPosition: TextView = itemView.findViewById(R.id.textViewPosition)
-		val textViewNumber: TextView = itemView.findViewById(R.id.textViewNumber)
-		val textViewAge: TextView = itemView.findViewById(R.id.textViewAge)
-		val textViewDateOfBirth: TextView = itemView.findViewById(R.id.textViewDateOfBirth)
+	class ViewHolder(private val binding: ListItemPlayerBinding) : RecyclerView.ViewHolder(binding.root) {
+		fun bind(item: Player) {
+			binding.player = item
+			binding.executePendingBindings()
+		}
 	}
 }
